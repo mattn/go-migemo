d07bac991c198b5219b7d686eef6e5caff38669f
mattn <mattn.jp@gmail.com>
2009-11-25 01:43:11 +0900

    add sample.

--- main.go
+++ main.go
@@ -1,9 +1,20 @@
 package main
 
-import "migemo"
+import (
+  "fmt";
+  "regexp";
+  "strings";
+  "migemo";
+)
 
 func main() {
+	var pattern = "goGengo";
+	var match = "go言語";
 	m := migemo.Open("../dict/utf-8.d/migemo-dict");
-	s := migemo.Query(m, "watasi");
-	println(s);
+	s := migemo.Query(m, pattern);
+	if (regexp.MustCompile(s).Match(strings.Bytes(match))) {
+	    fmt.Printf("%s は %s にマッチします！\n", pattern, match);
+	} else {
+	    fmt.Printf("%s は %s にマッチしません！\n", pattern, match);
+	}
 }
